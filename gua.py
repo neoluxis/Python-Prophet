c50fb0e8c4f2f5d86238b952a61cf8ac3a234a70
Neolux Lee <neolux.lee@foxmail.com>
2024-02-28 00:12:31 +0800

    rearrange files, update data, update date storage

--- gua.py
+++ gua.py
@@ -23,7 +23,7 @@ def unio():
     mu = 25
     sigma2 = 4
     # 因为人分蓍草总是习惯从中间分，所以把分两堆的数字设置为μ为25，σ²为4的正态分布
-    ran = [random.gauss(25, 4) for i in range(200)]
+    ran = [random.gauss(mu, sigma2) for i in range(200)]
     nums = [x for x in ran if 0 <= x <= 50]  # 大衍之数五十，所以筛选正态分布中小于50的数
 
     hold = 0  # 扐蓍之数
@@ -60,22 +60,30 @@ def gua():
     """
     res = []
     yao = []
+    bin_seq = []
     for i in range(6):
         une = unio()
         res.append(une)
         if une == 6:
             yao.append("太阴")
+            bin_seq.append(1)
         elif une == 7:
             yao.append("少阳")
+            bin_seq.append(1)
         elif une == 8:
             yao.append("少阴")
+            bin_seq.append(0)
         elif une == 9:
             yao.append("太阳")
-    print(res, yao)
-    return res, yao
+            bin_seq.append(0)
+    # print(res, yao, bin_seq)
+    value = 0
+    for i in range(-1, -7, -1):
+        value = value * 2 + bin_seq[i]
+    return res, yao, value
 
 
-def draw(yao=None, save=True, fname="Gua.png", show=False):
+def draw(yao=None, save=True, fname="images/gua.png", show=False):
     """
     绘制/保存/展示卦象图片
     """
@@ -84,16 +92,15 @@ def draw(yao=None, save=True, fname="Gua.png", show=False):
     # 定义图像大小和颜色
     image_size = (950, 950)  # 图像宽度和高度
     background_color = 255  # 白色背景颜色 (B, G, R)
-    fname = "gua.png"
 
-    ys = [850, 700, 550, 400, 250, 100]
-    startx = 100
-    midsx = 400
-    midex = 550
-    endx = 850
-    thickness = 100
+    ys = [850, 700, 550, 400, 250, 100]  # 爻的y坐标
+    startx = 100  # 爻的起始x坐标
+    midsx = 400  # 爻的中间x坐标
+    midex = 550  # 爻的中间x坐标
+    endx = 850  # 爻的结束x坐标
+    thickness = 100  # 爻的线宽
 
-    lcolor = (0, 0, 0)
+    lcolor = (0, 0, 0)  # 爻的颜色 (B, G, R)
 
     # 创建空白图像
     image = np.ones((image_size[1], image_size[0], 3),
@@ -101,9 +108,9 @@ def draw(yao=None, save=True, fname="Gua.png", show=False):
 
     # 在指定高度上绘制多条横线
     for i in range(6):
-        if yao[i] == 6 or yao[i] == 7:
+        if yao[i] == 6 or yao[i] == 7 or yao[i] == 1:  # 6, 7, 1 阳爻
             cv.line(image, (startx, ys[i]), (endx, ys[i]), lcolor, thickness)
-        else:
+        else:  # 8, 9, 0 阴爻
             cv.line(image, (startx, ys[i]), (midsx, ys[i]), lcolor, thickness)
             cv.line(image, (midex, ys[i]), (endx, ys[i]), lcolor, thickness)
 
@@ -112,6 +119,7 @@ def draw(yao=None, save=True, fname="Gua.png", show=False):
         cv.imwrite(fname, image)
         print(f"Image saved as {fname}")
 
+    # 展示图像
     if show:
         plt.imshow(image)
         plt.axis('on')
@@ -123,8 +131,9 @@ def main():
         int(round(time.time()) * 1000)  # 输入的字符串进行散列后与时间戳相加，构造独一无二的随机数种子
     # print(seed)
     random.seed(seed)
-    res, _ = gua()
-    draw(res)
+    res, yao, bsq = gua()
+    print(res, yao, bsq)
+    draw(res, show=True)
 
 
 if __name__ == '__main__':
